27d48cd47bc36d358f12b059417eca21849eeba5
José Córdova <josecordoba19@gmail.com>
2026-04-08 20:17:25 -0600

    Migracios, Modelos y Controladores terminados

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\MarcaController;
+use App\Http\Controllers\ClienteController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Ruta del contralador de producto
 Route::apiResource('productos', ProductoController::class);
 Route::apiResource('categorias', CategoriaController::class);
+Route::apiResource('marcas', MarcaController::class);
+Route::apiResource('clientes', ClienteController::class);
 
